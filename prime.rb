9d2eff48f347ba5511f8f6ee9e9140ae2919b0fd
Nia Bert <nbert14@gmail.com>
2019-04-30 00:25:42 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,8 +1,6 @@
 # Add  code here!
 def prime?(number)
-  (2..(number - 1)).each do |n|
-    return false if number % n == 0
-  end
-  true
+  (2..number-1).each {|n| return false if number <= 1 || number % n == 0}
+  return true
 
 end
